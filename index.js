8318e8dcf3fe988087d8ff75c84f543d1190f237
Jessica Shu <jessicashu7@gmail.com>
2020-06-08 23:52:18 -0700

    redesign website to one page

---
+++ index.js
@@ -0,0 +1,7 @@
+// use this command for local testing
+// chrome.exe --user-data-dir="C://Chrome dev session" --disable-web-security
+$( document ).ready(function() {
+  $("#aboutMeDiv").load("aboutme.html");
+  $("#workDiv").load("work.html");
+
+});
